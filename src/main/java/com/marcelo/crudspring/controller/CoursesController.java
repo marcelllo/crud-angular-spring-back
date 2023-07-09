cedae56d406b7593ffafabd96f878aa6a04b43de
Marcelo Santos <marcello.santos@gmail.com>
2023-07-09 19:00:36 -0300

    Criação do método de cadastro de cursos

--- src/main/java/com/marcelo/crudspring/controller/CoursesController.java
+++ src/main/java/com/marcelo/crudspring/controller/CoursesController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import com.marcelo.crudspring.model.Course;
 import com.marcelo.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -19,5 +19,12 @@ public class CoursesController {
   @GetMapping
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    return courseRepository.save(course);
+    // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
   }
 }
