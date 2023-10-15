cef3258cfd6630706a86284409b15fe0ebe3c389
Marcelo Santos <marcello.santos@gmail.com>
2023-10-14 23:12:13 -0300

    Update spring and jdk versions

--- src/main/java/com/marcelo/crudspring/model/Course.java
+++ src/main/java/com/marcelo/crudspring/model/Course.java
@@ -1,7 +1,11 @@
 package com.marcelo.crudspring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
