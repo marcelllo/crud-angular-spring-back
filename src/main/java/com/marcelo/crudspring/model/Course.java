1048c5add902d7e095e099ff27d64d90fbb70d2e
Marcelo Santos <marcello.santos@gmail.com>
2023-10-14 23:01:18 -0300

    Implementação do SoftDelete

--- src/main/java/com/marcelo/crudspring/model/Course.java
+++ src/main/java/com/marcelo/crudspring/model/Course.java
@@ -6,10 +6,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
+// @Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,9 +28,14 @@ public class Course {
     private String name;
 
     @NotNull
-    @NotBlank
     @Length(max = 10)
     @Pattern(regexp = "Back-End|Front-End")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
