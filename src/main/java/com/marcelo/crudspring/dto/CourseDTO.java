42e55ee8b9a382cd726c771413926f5a44fa50e7
Marcelo Santos <marcello.santos@gmail.com>
2023-10-16 17:18:28 -0300

    Ajuste na entidade Lesson para permitir listar cursos e lições.

--- src/main/java/com/marcelo/crudspring/dto/CourseDTO.java
+++ src/main/java/com/marcelo/crudspring/dto/CourseDTO.java
@@ -1,13 +1,18 @@
 package com.marcelo.crudspring.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.marcelo.crudspring.model.Lesson;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
+import java.util.List;
+
 public record CourseDTO(
     @JsonProperty("_id") Long id,
     @NotNull @NotBlank @Length(min = 5, max = 100) String name,
-    @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category
+    @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category,
+
+    List<Lesson> lessons
 ) {}
